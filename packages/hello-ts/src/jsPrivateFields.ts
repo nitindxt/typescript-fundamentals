6d0748f7b86ab7c83f58ac46889ff8ff9c23d11c
Nitin Dixit <nitinajaydixit@gmail.com>
2022-05-18 19:45:22 +0530

    check jsPrivate excercise

---
+++ packages/hello-ts/src/jsPrivateFields.ts
@@ -0,0 +1,17 @@
+class Car {
+  public make: string;
+  public model: string;
+  #year: number;
+
+  constructor(make: string, model: string, year: number) {
+    this.make = make;
+    this.model = model;
+    this.#year = year;
+  }
+
+  printPrivate = (): number => {
+    return this.#year;
+  };
+}
+const c = new Car("Honda", "Accord", 2017);
+console.log(c.printPrivate());
