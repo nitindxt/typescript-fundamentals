268336d345f329a8d091775887e2fe7c1d70d591
Nitin Dixit <nitinajaydixit@gmail.com>
2022-05-18 08:06:13 +0530

    jsonTypes exercise

---
+++ packages/hello-ts/src/typeImplements.ts
@@ -0,0 +1,15 @@
+// type CanBark =
+//   | number
+//   | {
+//       bark(): string;
+//     };
+
+// class Dog implements CanBark {
+//   //A class can only implement an object type or intersection of object types with statically known members.
+//   bark() {
+//     return "woof";
+//   }
+//   eat(food) {
+//     consumeFood(food);
+//   }
+// }
